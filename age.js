7836fc577653832d686332a0ddc32753d6a09501
ShahzaibAhmet <shahzaib.ahmet@gmail.com>
2024-02-27 01:35:26 +0500

    Initial commit

---
+++ age.js
@@ -0,0 +1,49 @@
+let userinput = document.getElementById('inputbox');
+userinput.max = new Date().toISOString().split("T")[0];
+let result = document.getElementById("idk");
+result.innerHTML = `Your are ${y3} years, ${m3} months and ${d3} days old`;
+
+
+
+function CalculateAge() {
+    let birthdate = new Date(userinput.value);
+
+    let d1 = birthdate.getDate();
+    let m1 = birthdate.getMonth() + 1;
+    let y1 = birthdate.getFullYear();
+
+
+    let today = new Date();
+
+
+    let d2 = today.getDate();
+    let m2 = today.getMonth() + 1;
+    let y2 = today.getFullYear();
+
+    let d3, m3, y3;
+
+    y3 = y2 - y1;
+
+    if (m2 >= m1) {
+        m3 = m2 - m1;
+    } else {
+        y3--;
+        m3 = 12 + m2 - m1;
+    }
+    if (d2 >= d1) {
+        d3 = d2 - d1
+    } else {
+        m3--;
+        d3 = getDaysInMonth(y1, m1) + d2 - d1;
+    }
+    if (m3 < 0) {
+        m3 = 11;
+        y3--;
+    }
+
+    result.innerHTML = `Your are <span>${y3}</span>  years, <span>${m3}</span> months and <span>${d3}</span> days old`;
+}
+
+function getDaysInMonth(Year, month) {
+    return new Date(Year, month, 0).getDate();
+}
